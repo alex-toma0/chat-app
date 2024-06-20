c37ef1d2a946eaff79a3017e3bed1ea1932d4383
Alex <alexandrutoma.ro@gmail.com>
2024-06-20 14:36:31 +0300

    Using react-use-websocket for client side websocket initialization

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,43 @@
-import Image from "next/image";
-
+"use client";
+import { useState, useEffect, useCallback } from "react";
+import useWebSocket, { ReadyState } from "react-use-websocket";
 export default function Home() {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState<string[]>([]);
+  const { sendMessage, lastMessage, readyState } = useWebSocket(
+    "ws://localhost:8080/",
+    {
+      onOpen: () => {
+        console.log("WebSocket connection established!");
+      },
+    }
+  );
+
+  useEffect(() => {
+    if (lastMessage != null) {
+      setMessages([...messages, lastMessage.data]);
+    }
+  }, [lastMessage]);
+
+  const sendMessageHandler = useCallback(() => sendMessage(message), []);
   return (
-    <div>
-      <span>this is a test</span>
+    <div className="h-full flex flex-col justify-center items-center">
+      <div className="chat chat-start">
+        {messages.length > 0 &&
+          messages.map((message) => (
+            <div className="chat-bubble" key={message + (message.length - 3)}>
+              {message}
+            </div>
+          ))}
+      </div>
+      <textarea
+        className="textarea"
+        placeholder="Type a message"
+        onChange={(e) => setMessage(e.target.value)}
+      ></textarea>
+      <button className="btn btn-primary max-w-sm" onClick={sendMessageHandler}>
+        Send
+      </button>
     </div>
   );
 }
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,18 @@
 import fastify from "fastify";
-
+import websocket from "@fastify/websocket";
 const server = fastify();
 
-server.get("/ping", async (request, reply) => {
-  return "pong\n";
+server.register(websocket);
+
+server.register(async function (server) {
+  // Define websocket routes
+  server.get("/", { websocket: true }, (socket, req) => {
+    socket.on("message", (message) => {
+      console.log(message.toString());
+      socket.send("hi from server" + new Date());
+    });
+    socket.send("The conversation started!");
+  });
 });
 
 server.listen({ port: 8080 }, (err, address) => {
